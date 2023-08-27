2845f064b41d1521edb075d6b1f3bebc4bbe57bb
ktunprasert <ktunprasert@outlook.com>
2023-08-27 15:44:05 +0100

    diff of squares

---
+++ difference-of-squares/difference_of_squares.go
@@ -0,0 +1,23 @@
+package diffsquares
+
+func SquareOfSum(n int) int {
+	var total int
+	for i := 0; i <= n; i++ {
+		total += i
+	}
+
+	return total * total
+}
+
+func SumOfSquares(n int) int {
+	var total int
+	for i := 0; i <= n; i++ {
+		total += i * i
+	}
+
+	return total
+}
+
+func Difference(n int) int {
+    return SquareOfSum(n) - SumOfSquares(n)
+}

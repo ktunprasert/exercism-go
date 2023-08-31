5ff93592412b1ce4de047f2ba307e46acde86875
ktunprasert <ktunprasert@outlook.com>
2023-08-31 17:42:08 +0100

    nth-primes: moved 'primes' outside func

--- nth-prime/nth_prime.go
+++ nth-prime/nth_prime.go
@@ -4,13 +4,13 @@ import (
 	"errors"
 )
 
+var primes []int = []int{2, 3, 5, 7, 11, 13}
+
 // Nth returns the nth prime number. An error must be returned if the nth prime number can't be calculated ('n' is equal or less than zero)
 func Nth(n int) (int, error) {
 	if n < 1 {
 		return -1, errors.New("n must be greater than zero")
 	}
-
-	var primes []int = []int{2, 3, 5, 7, 11, 13}
 
 	current := primes[len(primes)-1]
 	for n >= len(primes) {

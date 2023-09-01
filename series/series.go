257bf1db7c01d703776042e6d8acef1adef530c9
ktunprasert <ktunprasert@outlook.com>
2023-09-01 17:26:14 +0100

    series

---
+++ series/series.go
@@ -0,0 +1,18 @@
+package series
+
+func All(n int, s string) []string {
+	if n > len(s) {
+		return nil
+	}
+
+	result := make([]string, 0)
+	for i := 0; i < len(s)+1-n; i++ {
+		result = append(result, s[i:i+n])
+	}
+
+	return result
+}
+
+func UnsafeFirst(n int, s string) string {
+	return s[:n]
+}

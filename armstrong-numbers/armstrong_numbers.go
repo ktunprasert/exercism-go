26fa793151a2096b5f78a42217eacb0eea318404
ktunprasert <ktunprasert@outlook.com>
2023-09-01 16:54:29 +0100

    armstrong: guardcase < 10

--- armstrong-numbers/armstrong_numbers.go
+++ armstrong-numbers/armstrong_numbers.go
@@ -3,10 +3,11 @@ package armstrong
 import "math"
 
 func IsNumber(n int) bool {
-	digits := float64(1)
-	if n >= 10 {
-		digits = math.Floor(math.Log10(float64(n)) + 1)
+	if n < 10 {
+		return true
 	}
+
+	digits := math.Floor(math.Log10(float64(n)) + 1)
 
 	cpy := n
 	sum := 0

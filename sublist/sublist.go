64f30f7d48cc8faec1a73ffef79c25a1be77891c
ktunprasert <ktunprasert@outlook.com>
2023-09-01 20:45:46 +0100

    sublist

---
+++ sublist/sublist.go
@@ -0,0 +1,49 @@
+package sublist
+
+func Sublist(l1, l2 []int) Relation {
+	switch {
+	case len(l1) == 0 && len(l2) == 0:
+		return RelationEqual
+	case len(l1) == 0 && len(l2) > 0:
+		return RelationSublist
+	case len(l1) > 0 && len(l2) == 0:
+		return RelationSuperlist
+	}
+
+	switch {
+	case len(l1) == len(l2):
+		if subset(l1, l2) {
+			return RelationEqual
+		}
+	case len(l1) < len(l2):
+		if subset(l2, l1) {
+			return RelationSublist
+		}
+	case len(l1) > len(l2):
+		if subset(l1, l2) {
+			return RelationSuperlist
+		}
+	}
+
+	return RelationUnequal
+}
+
+func subset(big, small []int) bool {
+	for i := 0; i <= len(big)-len(small); i++ {
+		window := big[i : i+len(small)]
+
+		var eq int
+		for i, r := range window {
+			if r != small[i] {
+				break
+			}
+			eq++
+		}
+
+		if eq == len(small) {
+			return true
+		}
+	}
+
+	return false
+}

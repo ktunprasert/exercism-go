13e1d1eb05fd95193addf1cb37b096fda766a57b
ktunprasert <ktunprasert@outlook.com>
2023-09-05 11:28:58 +0100

    meetup: const as actual date addition

--- meetup/meetup.go
+++ meetup/meetup.go
@@ -8,26 +8,26 @@ import (
 type WeekSchedule int
 
 const (
-	Last   = -2
-	Teenth = -1
-	First  = 0
-	Second = 1
-	Third  = 2
-	Fourth = 3
-	Fifth  = 4
+	First = iota * 7
+	Second
+	Third
+	Fourth
+	Fifth
+
+	Teenth = 12
+	Last   = -1
 )
 
 func Day(wSched WeekSchedule, wDay time.Weekday, month time.Month, year int) int {
 	start := time.Date(year, month, 1, 0, 0, 0, 0, time.UTC)
 	dayModifier := 1
+
 	switch wSched {
 	case Last:
 		start = start.AddDate(0, 1, -1)
 		dayModifier = -1
-	case Teenth:
-		start = start.AddDate(0, 0, 12)
 	default:
-		start = start.AddDate(0, 0, int(wSched)*7)
+		start = start.AddDate(0, 0, int(wSched))
 	}
 
 	for start.Weekday() != wDay {

6371435207bf53bcaed0c756469beb44a3596c37
ktunprasert <ktunprasert@outlook.com>
2023-09-06 09:29:20 +0100

    flatten-array: recursion

---
+++ flatten-array/flatten_array.go
@@ -0,0 +1,17 @@
+package flatten
+
+func Flatten(nested interface{}) []interface{} {
+	flat := make([]interface{}, 0)
+
+	switch nested.(type) {
+	case []interface{}:
+		for _, v := range nested.([]interface{}) {
+			flat = append(flat, Flatten(v)...)
+		}
+
+	case interface{}:
+		flat = append(flat, nested)
+	}
+
+	return flat
+}

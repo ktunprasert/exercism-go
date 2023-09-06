a2cd63f5d29862d7c52ac379d2aa60464195d92b
ktunprasert <ktunprasert@outlook.com>
2023-09-06 17:57:41 +0100

    allergies: switch case

--- allergies/allergies.go
+++ allergies/allergies.go
@@ -1,6 +1,6 @@
 package allergies
 
-var allergens = []string{
+var allergens = [8]string{
 	"eggs",
 	"peanuts",
 	"shellfish",
@@ -14,23 +14,34 @@ var allergens = []string{
 func Allergies(allergies uint) []string {
 	res := []string{}
 
-	allergies = allergies % 256
-
 	for i := 0; i < len(allergens); i++ {
-        if allergies & (1 << uint(i)) != 0 {
-            res = append(res, allergens[i])
-        }
+		if AllergicTo(allergies, allergens[i]) {
+			res = append(res, allergens[i])
+		}
 	}
 
 	return res
 }
 
 func AllergicTo(allergies uint, allergen string) bool {
-    for i, item := range allergens {
-        if item == allergen {
-            return allergies & (1 << uint(i)) != 0
-        }
-    }
+	switch allergen {
+	case "eggs":
+		return allergies&1 != 0
+	case "peanuts":
+		return allergies&2 != 0
+	case "shellfish":
+		return allergies&4 != 0
+	case "strawberries":
+		return allergies&8 != 0
+	case "tomatoes":
+		return allergies&16 != 0
+	case "chocolate":
+		return allergies&32 != 0
+	case "pollen":
+		return allergies&64 != 0
+	case "cats":
+		return allergies&128 != 0
+	}
 
-    return false
+	return false
 }

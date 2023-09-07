efa5e9775a29f63121e738be01191763b22dbc2c
ktunprasert <ktunprasert@outlook.com>
2023-09-07 18:00:54 +0100

    diamond: index based solution & double add

--- diamond/diamond.go
+++ diamond/diamond.go
@@ -1,6 +1,7 @@
 package diamond
 
 import (
+	"bytes"
 	"errors"
 	"strings"
 )
@@ -14,35 +15,20 @@ func Gen(char byte) (string, error) {
 		return "A", nil
 	}
 
-	lineWidth := 1 + 2*(rune(char)-'A')
-	s := strings.Builder{}
+	lineWidth := 1 + 2*(char-'A')
+	out := make([]string, lineWidth)
 
-	for i := 'A'; i <= rune(char); i++ {
-		middle := renderChar(i)
-		spaces := (int(lineWidth) - len(middle)) / 2
-		s.WriteString(strings.Repeat(" ", int(spaces)))
-		s.WriteString(renderChar(i))
-		s.WriteString(strings.Repeat(" ", int(spaces)))
-		s.WriteString("\n")
+	for c := byte('A'); c <= char; c++ {
+		row := bytes.Repeat([]byte{' '}, int(lineWidth))
+		colNum := char - c
+
+		row[colNum] = c
+		row[lineWidth-colNum-1] = c
+
+		rowNum := c - 'A'
+		out[rowNum] = string(row)
+		out[lineWidth-rowNum-1] = string(row)
 	}
 
-	for i := rune(char) - 1; i >= 'A'; i-- {
-		middle := renderChar(i)
-		spaces := (int(lineWidth) - len(middle)) / 2
-		s.WriteString(strings.Repeat(" ", int(spaces)))
-		s.WriteString(renderChar(i))
-		s.WriteString(strings.Repeat(" ", int(spaces)))
-		s.WriteString("\n")
-	}
-
-	return s.String()[:len(s.String())-1], nil
-}
-
-func renderChar(c rune) string {
-	if c == 'A' {
-		return "A"
-	}
-
-	dist := c - 'A'
-	return string(c) + strings.Repeat(" ", 2*(int(dist)-1)+1) + string(c)
+	return strings.Join(out, "\n"), nil
 }

396c6a66787e1ddef6e671b5299882b66f54586c
ktunprasert <ktunprasert@outlook.com>
2023-09-08 13:21:45 +0100

    yacht: optimised

--- yacht/yacht.go
+++ yacht/yacht.go
@@ -10,72 +10,56 @@ func Score(dice []int, category string) int {
 		diceCount[d]++
 	}
 
-	switch {
-	case category == "ones":
+	switch category {
+	case "ones":
 		return diceCount[1]
-	case category == "twos":
+	case "twos":
 		return diceCount[2] * 2
-	case category == "threes":
+	case "threes":
 		return diceCount[3] * 3
-	case category == "fours":
+	case "fours":
 		return diceCount[4] * 4
-	case category == "fives":
+	case "fives":
 		return diceCount[5] * 5
-	case category == "sixes":
+	case "sixes":
 		return diceCount[6] * 6
-	case category == "full house" && len(diceCount) == 2:
-		var condThree, condTwo bool
+	case "full house":
 		sum := 0
 		for k, v := range diceCount {
-			if v == 3 {
-				condThree = true
-				sum += k * v
+			if v != 2 && v != 3 {
+				return 0
 			}
 
-			if v == 2 {
-				condTwo = true
-				sum += k * v
-			}
+			sum += k * v
 		}
 
-		if condThree && condTwo {
-			return sum
+		return sum
+	case "little straight":
+		if len(diceCount) == 5 && diceCount[6] == 0 {
+			return 30
 		}
-	case category == "little straight" &&
-		diceCount[1] > 0 &&
-		diceCount[2] > 0 &&
-		diceCount[3] > 0 &&
-		diceCount[4] > 0 &&
-		diceCount[5] > 0:
-		return 30
-	case category == "big straight" &&
-		diceCount[2] > 0 &&
-		diceCount[3] > 0 &&
-		diceCount[4] > 0 &&
-		diceCount[5] > 0 &&
-		diceCount[6] > 0:
-		return 30
-	case category == "choice":
+	case "big straight":
+		if len(diceCount) == 5 && diceCount[1] == 0 {
+			return 30
+		}
+	case "choice":
 		sum := 0
 		for k, v := range diceCount {
 			sum += k * v
 		}
+
 		return sum
-	case len(diceCount) == 1:
-		if category == "yacht" {
-			return 50
+	case "yacht":
+		if diceCount[dice[0]] != 5 {
+			return 0
 		}
-		if category == "four of a kind" {
-			return dice[0] * 4
-		}
-	case len(diceCount) < 3:
-		sum := 0
+		return 50
+	case "four of a kind":
 		for k, v := range diceCount {
-			if v == 4 {
-				sum += k * v
+			if v >= 4 {
+				return 4 * k
 			}
 		}
-		return sum
 	}
 
 	return 0
